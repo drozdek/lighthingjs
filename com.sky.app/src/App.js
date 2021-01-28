ec7aafd06df8ffada32b54c67b500d1f5851f844
Rafal <raf.drozdowski@gmail.com>
2021-01-28 18:19:06 +0000

    defines properties

--- com.sky.app/src/App.js
+++ com.sky.app/src/App.js
@@ -6,9 +6,9 @@ export default class App extends Lightning.Component {
   _construct() {
   }
 
-  _onActive(){
-    console.log('I am visible');
-  }
+  // _onActive(){
+  //   console.log('I am visible');
+  // }
 
   _handleLeft() {
     this.handleLeft();
@@ -24,138 +24,83 @@ export default class App extends Lightning.Component {
   }
 
   handleLeft() {
-    console.log('left')
+    let elem = this.tag('LetterList');
+    let elemPosX = Math.ceil(elem.getSmooth('x') - 50);
+    elem.setSmooth("x", elemPosX);
+    this.currentPosX = elemPosX;
+    console.log('left' + elemPosX)
   }
-  
+
   handleRight() {
-    console.log('right')
+    let elem = this.tag('LetterList');
+    let elemPosX = Math.ceil(elem.getSmooth('x') + 50);
+    elem.setSmooth("x", elemPosX);
+    this.currentPosX = elemPosX;
+    console.log('right' + elemPosX)
+  }
+
+  getLetter(){
+
   }
 
   static _template() {
-    this.y = 50
-
+    this.y = 70;
+    this.color= 0xff005500;
+    this.textColor = 0xaaaaaaaa;
+    this.textAlign = 'center'
     return {
-      Content: {
-        Header: {
-          y: 20,
-          text: {
-            text: 'Press an arrow key to select a letter...',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'left',
-            fontSize: 30
-          }
-        },
+      Header: {
+        y: 20, text: { text: 'Press an arrow key to select a letter...', textColor: 0xaa000000, textAlign: 'left', fontSize: 30 }
+      },      
+      LetterList: {
         A: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 10,
-          y: this.y,
-          text: {
-            text: 'a',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
-          }
+          rect: true, w: 50, h: 50, color: this.color, x: 0, y: this.y, text: { text: 'a', color: this.textColor, textAlign: this.textAlign, fontSize: 30 }
         },
         B: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          border: true,
-          x: 60,
-          y: this.y,
-          text: {
-            text: 'b',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, border: true, x: 50, y: this.y, text: {
+            text: 'b', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         C: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 110,
-          y: this.y,
-          text: {
-            text: 'c',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 100, y: this.y, text: {
+            text: 'c', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         D: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 160,
-          y: this.y,
-          text: {
-            text: 'd',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 150, y: this.y, text: {
+            text: 'd', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         E: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 210,
-          y: this.y,
-          text: {
-            text: 'e',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 200, y: this.y, text: {
+            text: 'e', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         F: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 260,
-          y: this.y,
-          text: {
-            text: 'f',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 250, y: this.y, text: {
+            text: 'f', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         G: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 310,
-          y: this.y,
-          text: {
-            text: 'g',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 300, y: this.y, text: {
+            text: 'g', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         H: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 360,
-          y: this.y,
-          text: {
-            text: 'h',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 350, y: this.y, text: {
+            text: 'h', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         I: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 410,
-          y: this.y,
-          text: {
-            text: 'i',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 400, y: this.y, text: {
+            text: 'i', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
         },
         J: {
-          rect: true, w: 50, h: 50, color: 0xff005500,
-          x: 460,
-          y: this.y,
-          text: {
-            text: 'j',
-            textColor: 0xaaaaaaaa,
-            textAlign: 'center',
-            fontSize: 30
+          rect: true, w: 50, h: 50, color: this.color, x: 450, y: this.y, text: {
+            text: 'j', color: this.textColor, textAlign: this.textAlign, fontSize: 30
           }
-        },                                                        
+        },
       }
     }
   };
